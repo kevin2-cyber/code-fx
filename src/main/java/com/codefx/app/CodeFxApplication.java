54129c9e0a470b1c4bfd88d6ede55f74a2152dbd
BAPANAYE20 <bapanaye2019@outlook.com>
2023-06-27 10:28:27 +0100

    edited app name

--- src/main/java/com/codefx/app/CodeFxApplication.java
+++ src/main/java/com/codefx/app/CodeFxApplication.java
@@ -7,11 +7,11 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class CodeFx extends Application {
+public class CodeFxApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(CodeFx.class.getResource("main.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(CodeFxApplication.class.getResource("main.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
         stage.setTitle("CodeFx Pharmacy Management System");
         stage.setScene(scene);
