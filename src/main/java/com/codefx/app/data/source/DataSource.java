82941d61c1a898d0a6a282b6164e2bf65a49ac02
Kelvin Eduful <keduful@st.ug.edu.gh>
2023-08-05 10:11:36 +0000

    unnecesarry thing to do

--- src/main/java/com/codefx/app/data/source/DataSource.java
+++ src/main/java/com/codefx/app/data/source/DataSource.java
@@ -2,4 +2,16 @@ package com.codefx.app.data.source;
 
 import com.codefx.app.repository.Repository;
 
-public interface DataSource<T> extends Repository<T> {}
+import java.util.List;
+
+public interface DataSource<T> extends Repository<T> {
+
+    @Override
+    void save(T model);
+
+    @Override
+    T retrieve(int id);
+
+    @Override
+    List<T> retrieveAll();
+}
