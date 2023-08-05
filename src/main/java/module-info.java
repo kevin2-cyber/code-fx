c59d93cb61dab614b0fca4bd7c11fd915b5715db
Kelvin Eduful <keduful@st.ug.edu.gh>
2023-08-05 13:28:54 +0000

    added mysql connector

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.codefx.codefx {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires mysql.connector.j;
 
 
     opens com.codefx.app to javafx.fxml;
