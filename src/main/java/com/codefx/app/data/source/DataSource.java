585a47618a8f1e2902607dae268cf45309d0a7a4
Kelvin Eduful <keduful@st.ug.edu.gh>
2023-08-05 10:12:49 +0000

    making the data source

--- src/main/java/com/codefx/app/data/source/DataSource.java
+++ src/main/java/com/codefx/app/data/source/DataSource.java
@@ -1,17 +1,3 @@
 package com.codefx.app.data.source;
 
-import com.codefx.app.repository.Repository;
-
-import java.util.List;
-
-public interface DataSource<T> extends Repository<T> {
-
-    @Override
-    void save(T model);
-
-    @Override
-    T retrieve(int id);
-
-    @Override
-    List<T> retrieveAll();
-}
+public interface DataSource{}
